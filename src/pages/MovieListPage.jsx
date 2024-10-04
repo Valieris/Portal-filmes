7a71b3e2c05e431f09daec9aa1c544804d758832
Valieris <enzovalieris12@gmail.com>
2024-10-03 21:36:33 -0300

    novo

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -25,11 +25,14 @@ export default function MovieListPage() {
             />
             <section className="flex">
                 {
-                    
+                    filmesFiltrados.length > 0 ?
+
                     filmesFiltrados
                         .map(filme => (
                             <MovieCard key={filme.id} {...filme} />
                         ))
+                    :
+                    <p>Filme não encontrado</p>
                 }
             </section>
         </>
