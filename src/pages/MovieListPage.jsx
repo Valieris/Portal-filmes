c8a32c31da5943a32248aeadd0b2b5c3f480a5e4
Valieris <enzovalieris12@gmail.com>
2024-10-10 21:02:57 -0300

    novo commit

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
 import movies from "../data/movies.json"
 
 export default function MovieListPage() {
 
     const [search, setSearch] = useState("")
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br')
+            .then( res => res.json())
+            .then(data => setFilmes(data.results))
+            .catch(erro => console.log(erro))
+            .finally(()=> console.log("FIM!"))
+    },[])
+
 
     const handleSearch = (event) => {
         setSearch(event.target.value)
         console.log(search)
     }
 
-    const filmesFiltrados = movies.filter( filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
+    const filmesFiltrados = movies.filter(filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <>
@@ -25,14 +35,14 @@ export default function MovieListPage() {
             />
             <section className="flex">
                 {
-                    filmesFiltrados.length > 0 ?
-
-                    filmesFiltrados
-                        .map(filme => (
-                            <MovieCard key={filme.id} {...filme} />
-                        ))
-                    :
-                    <p>Filme não encontrado</p>
+                    filmes.map(filme => (
+                    <>    
+                        <h1>{filme.title}</h1>
+                        <p>{filme.vote_average}</p>
+                        <img src={`https://image.tmdb.org/t/p/w154${filme.poster_path}`} alt={filme.title}/>
+                        <img src={`https://image.tmdb.org/t/p/w154${filme.poster_path}`} alt={filme.title}/>
+                    </>
+                    ) )
                 }
             </section>
         </>
